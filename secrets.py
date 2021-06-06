e9fcb55c814609f94d9b63c374a2280c39d7395b
michaelkozin <62798503+michaelkozin@users.noreply.github.com>
2021-06-06 13:07:44 +0300

    Delete secrets.py

--- secrets.py
+++
@@ -1,9 +0,0 @@
-secret_key = "michaelkmichael"
-
-Host = "ec2-54-87-112-29.compute-1.amazonaws.com"
-Database_name = "dbi8jtb4fm82j6"
-User = "wjsdqozngyzbip"
-Port = 5432
-Password = "a9f7dafc9543e3e003cea2d6d8a1dff06fff47fb4817db61d1d91540b56e7dd8"
-_MK_URI = "postgres://wjsdqozngyzbip:a9f7dafc9543e3e003cea2d6d8a1dff06fff47fb4817db61d1d91540b56e7dd8@ec2-54-87-112-29.compute-1.amazonaws.com:5432/dbi8jtb4fm82j6"
-Heroku_CLI = "heroku pg:psql postgresql-convex-81664 --app setting-goals"
